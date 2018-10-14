56815ad2aae6e4e5e032a42544899f6fabcf283d
Miguel Cejas <mbcrfull@gmail.com>
2018-10-14 18:08:42 -0300

    Agregar Hola mundo

---
+++ src/ejer/pkg1/tp3/Ejer1TP3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejer.pkg1.tp3;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Ejer1TP3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo");
+    }
+    
+}
