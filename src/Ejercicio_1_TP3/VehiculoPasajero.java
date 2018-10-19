a7c35664dcc1964d28e74416e0ef561d679eade6
Miguel Cejas <mbcrfull@gmail.com>
2018-10-19 04:44:34 -0300

    Sistema Funcionando

--- src/Ejercicio_1_TP3/VehiculoPasajero.java
+++ src/Ejercicio_1_TP3/VehiculoPasajero.java
@@ -1,13 +1,21 @@
 package Ejercicio_1_TP3;
 
 public class VehiculoPasajero extends Vehiculo {
-
+    private Integer plazas;
+    
     public VehiculoPasajero() {
     }
 
-    public VehiculoPasajero(String marca, String patente, Double precioBase) {
+    public VehiculoPasajero(String marca, String patente, Double precioBase, Integer plazas) {
         super(marca, patente, precioBase);
+        this.plazas=plazas;
     }
-    
-    
+
+    public Integer getPlazas() {
+        return plazas;
+    }
+
+    public void setPlazas(Integer plazas) {
+        this.plazas = plazas;
+    }
 }
