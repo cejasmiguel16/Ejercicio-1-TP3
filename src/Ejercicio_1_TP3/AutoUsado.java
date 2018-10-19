adaebd586b7e8c4b7e9f4296c60da9cd422ebc56
Miguel Cejas <mbcrfull@gmail.com>
2018-10-19 03:31:41 -0300

    Agregue el metodo de comprar para los autos viejos

--- src/Ejercicio_1_TP3/AutoUsado.java
+++ src/Ejercicio_1_TP3/AutoUsado.java
@@ -1,18 +1,27 @@
 package Ejercicio_1_TP3;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AutoUsado extends Auto implements Alquiler{
+public class AutoUsado extends Auto implements Alquiler, Venta {
 
     Scanner entrada=new Scanner(System.in);
+    private ArrayList<Componente> componentes;
     
     public AutoUsado() {
+        componentes=new ArrayList<Componente>();
     }
 
     public AutoUsado(String marca, String patente, Double precioBase) {
         super(marca, patente, precioBase);
+        componentes=new ArrayList<Componente>();
     }
-
+    
+       public void IngresarComponentes(Componente componente){
+        componentes.add(componente);
+    }
+    
+    
     @Override
     public void calcularPrecioAlquiler() {
         Double precioTotal;
@@ -25,6 +34,18 @@ public class AutoUsado extends Auto implements Alquiler{
         precioTotal=(plazas*50)+(dias*50)+(getPrecioBase()*0.35)+getPrecioBase();
         System.out.println("Trato hecho");
         System.out.println("El precio de tu alquiler es de :"+precioTotal);
+    }
+
+    @Override
+    public void calcularPrecioVenta() {
+         Double porcentaje, precio=getPrecioBase()+((50*getPrecioBase())/100);
+            for(Componente i: componentes){
+                if(i!=null){
+                porcentaje= ((i.getPorcentaje()*getPrecioBase())/100);
+                precio= precio + porcentaje;
+                System.out.println("PRECIO: "+precio);
+            }        
+        }
     }
     
 }
