28f783b6778e395de6ce0af90a6d7315f413b75c
Miguel Cejas <mbcrfull@gmail.com>
2018-10-19 04:50:12 -0300

    AGREGANDO COMENTARIOS

--- src/Ejercicio_1_TP3/Principal.java
+++ src/Ejercicio_1_TP3/Principal.java
@@ -11,26 +11,33 @@ public class Principal {
      */
     public static void main(String[] args) {
         Flota flota= new Flota();
-        Vehiculo v= new AutoNuevo("Toyota", "XTZ200", 2000.);
-        AutoNuevo autoNuevo = (AutoNuevo) v;
-        Componente c1= new Componente("Aire", 2.0);
-        autoNuevo.IngresarComponentes(c1);
-        autoNuevo.IngresarComponentes(new Componente("LevantaCristales", 5.0));
-        flota.ingresarVehiculo(v);
+        
+        //INGRESANDO AUTO NUEVO Y AGREGANDO COMPONENTES PARA LA VENTA DEL MISMO
+        
+        //Vehiculo v= new AutoNuevo("Toyota", "XTZ200", 2000.);
+        //AutoNuevo autoNuevo = (AutoNuevo) v;
+        //Componente c1= new Componente("Aire", 2.0);
+        //autoNuevo.IngresarComponentes(c1);
+        //autoNuevo.IngresarComponentes(new Componente("LevantaCristales", 5.0));
+        //flota.ingresarVehiculo(v);
         //flota.comprarAutoNuevo("XTZ200");
         
         
-        flota.ingresarVehiculo(new Camion("Iveco", "TYO567", 400.));
-        flota.ingresarVehiculo(new Camioneta("Toyota","TGB500", 500.));
+       //INGRESANDO CAMION Y CAMIONETA A LA FLOTA PARA ALQUILAR
+        
+        //flota.ingresarVehiculo(new Camion("Iveco", "TYO567", 400.));
+        //flota.ingresarVehiculo(new Camioneta("Toyota","TGB500", 500.));
         //flota.alquilarCamioneta("TGB500");
         //flota.alquilarCamion("TYO560");
         
-        
-       flota.ingresarVehiculo(new AutoUsado("Renault","PFD405", 600., 4));
-       flota.ingresarVehiculo(new Minibus("Ferrari", "TORASV", 9));
+       
+       //INGRESANDO AUTO USADO Y MINIBUS PARA VENDER Y ALQUILAR
+       
+       //flota.ingresarVehiculo(new AutoUsado("Renault","PFD405", 600., 4));
+       //flota.ingresarVehiculo(new Minibus("Ferrari", "TORASV", 9));
        //flota.alquilarAutoUsado("PFD405");
        //flota.comprarAutoUsado("PFD405");
-       flota.alquilarMinibus("TORASV");
+       // flota.alquilarMinibus("TORASV");
     }
     
 }
